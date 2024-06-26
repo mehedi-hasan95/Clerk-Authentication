d49b9a41c2ea1bdf4cbec0d0fa3a060e6e26b7c8
mehedi-hasan95 <mh.me.mehedi@gmail.com>
2024-06-26 14:49:42 +0600

    Clerk authintication

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,10 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
+  const user = await currentUser();
+  if (user) redirect("/");
+  return <div className="max-w-screen-2xl mx-auto px-6">{children}</div>;
+};
+
+export default AuthLayout;
